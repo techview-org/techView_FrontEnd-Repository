2333dae8f326134d03cdd73a43b5a0d0fd866267
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-11-29 14:30:39 -0500

    test posts and sidebar added

---
+++ tech-view/src/components/LikeAndCommentButtonContainer.js
@@ -0,0 +1,9 @@
+export default function LikeAndCommentButtonContainer () {
+  return (
+  // style added was purely for testing purposes
+    <div style={{ border: '1px solid black' }}>
+      <button>Like</button>
+      <button>Comment</button>
+    </div>
+  )
+}
---
+++ tech-view/src/components/MainPage.js
@@ -0,0 +1,11 @@
+import PostCardsContainer from './PostCardsContainer'
+import SideBar from './SideBar'
+
+export default function MainPage () {
+  return (
+    <>
+      <SideBar />
+      <PostCardsContainer />
+    </>
+  )
+}
