e98b1c54c7ed9d79bff51a03844a8c295391e620
EmmanuelR21 <ruiz.emmanuel135@gmail.com>
2022-11-30 11:01:14 -0500

    moved components into one file, added Navbar and Footer to MainPage

--- tech-view/src/components/MainPage.js
+++ tech-view/src/components/MainPage.js
@@ -1,11 +1,15 @@
+import Navbar from './Navbar'
 import PostCardsContainer from './PostCardsContainer'
 import SideBar from './SideBar'
+import Footer from './Footer'
 
 export default function MainPage () {
   return (
     <>
+      <Navbar />
       <SideBar />
       <PostCardsContainer />
+      <Footer />
     </>
   )
 }
