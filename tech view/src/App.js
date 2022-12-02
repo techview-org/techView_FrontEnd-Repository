7939317be253a172d614cd8b0aad358578a8f43d
marcwayn8 <juniormarch48@gmail.com>
2022-12-02 07:54:07 -0500

    feat: post UI added

--- tech view/src/App.js
+++ tech view/src/App.js
@@ -2,23 +2,12 @@ import Navbar from "./community board/components/Navbar";
 import Footer from "./community board/components/Footer";
 import React from 'react';
 import Community from "./community board/components/Community";
-import { useState, createContext } from "react";
+import React from "react";
 import * as Unicons from '@iconscout/react-unicons';
 
 
 export const AppContext = createContext(null);
 
-const UseInputValueForFetch = (event) => {
-  const [text, getInput] = useState("");//onchange
-  event.preventDefault()
-  let textFromForm = event.target.value;
-  getInput(textFromForm)
-}
-
-const ProcessInputUsingButton = () => {
-  const [text, setInput] = useState("");//buttons
-  setInput(text)
-}
 
 function App() {
   return (
