0b282011b6e52b045f75d3a28938ecd294f41298
marcwayn8 <juniormarch48@gmail.com>
2022-12-05 11:37:01 -0500

    refactored code

--- tech view/routes/commentRoutes.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const commentController = require('../controllers/commentController.js');
-
-router.get('/', commentController.getComments);
-router.post('/', commentController.addComment)
-
-
-module.exports = router;
--- tech view/src/App.js
+++ tech view/src/App.js
@@ -46,6 +46,7 @@ const App = () => {
   return (
   
     <Container maxWidth="lg">
+
     <Cards details={details} />
       <AppBar className={classes.appBar} position="static" color="inherit">
         <Typography className={classes.heading} variant="h2" align="center">Posts</Typography>
