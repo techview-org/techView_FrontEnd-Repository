54c43f85febcd3564c86e49f721f93adf0e35746
marcwayn8 <juniormarch48@gmail.com>
2022-12-15 00:31:49 -0500

    fix:old components added to retired components folder and borrowed components added to new components folder

--- tech-view/src/components/retiredComponents/commentModal.js
+++ tech-view/src/components/retiredComponents/commentModal.js
@@ -4,6 +4,18 @@ import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 import Modal from '@mui/material/Modal';
 import NewCommentModal from './newCommentModal';
+import {
+    ChatBubbleLeftEllipsisIcon,
+    CodeBracketIcon,
+    EllipsisVerticalIcon,
+    EyeIcon,
+    FlagIcon,
+    HandThumbUpIcon,
+    MagnifyingGlassIcon,
+    PlusIcon,
+    ShareIcon,
+    StarIcon,
+  } from '@heroicons/react/20/solid'
 
 const style = {
     position: 'absolute',
@@ -29,11 +41,9 @@ function CommentModal(props) {
     const handleOpen = () => setOpen(true);
     const handleClose = () => setOpen(false);
 
-
-
     return (
         <div>
-            <Button style={button} onClick={handleOpen} onChange={(e) => { e.preventDefault() }}>Comment</Button>
+            <Button style={button} onClick={handleOpen} onChange={(e) => { e.preventDefault() }}></Button>
             <Modal
                 open={open}
                 onClose={handleClose}
